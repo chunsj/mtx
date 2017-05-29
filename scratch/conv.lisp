17c992bf7248633167c518b65586ea848334e9a5
Sungjin Chun <chunsj@gmail.com>
2017-05-29 20:24:19 +0900

    very inefficient convolution, i hate this

--- scratch/conv.lisp
+++ scratch/conv.lisp
@@ -7,7 +7,16 @@
        (f ($m '((2 0 1)
                 (0 1 2)
                 (1 0 2)))))
-  ($convolute m f 3))
+  ($convolute m f))
+
+(let* ((m ($m '((1 2 3 0)
+                (0 1 2 3)
+                (3 0 1 2)
+                (2 3 0 1))))
+       (f ($m '((2 0 1)
+                (0 1 2)
+                (1 0 2)))))
+  ($convolute m f :padding 1 :stride 3))
 
 (defparameter *mnist* (read-mnist-data))
 
